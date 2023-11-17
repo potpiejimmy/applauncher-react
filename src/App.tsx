0797f112f7f7e1c0a3b008bca71173ec883f4750
potpiejimmy <potpiejimmy@gmail.com>
2023-11-17 21:07:39 +0100

    ctx, state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import Button from '@mui/material/Button';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -9,43 +10,61 @@ import CloudIcon from '@mui/icons-material/Cloud';
 import BrightnessAutoIcon from '@mui/icons-material/BrightnessAuto';
 import InfoIcon from '@mui/icons-material/Info';
 import FullScreenIcon from '@mui/icons-material/Fullscreen';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './App.css';
 
+const AppCtx = React.createContext({});
+
+const theme = createTheme({
+    palette: {
+      primary: { main: "#666" }
+    },
+});
+
 function App() {
-  return (
-    <div>
 
-      <Box>
-          <AppBar position="static">
-              <Toolbar className="space-x-5">
-                  <IconButton size="small" color="inherit" aria-label="add">
-                      <AddIcon />
-                  </IconButton>
-                  <IconButton size="small" color="inherit" aria-label="order">
-                      <ReorderIcon></ReorderIcon>
-                  </IconButton>
-                  <IconButton size="small" color="inherit" aria-label="cloud">
-                      <CloudIcon></CloudIcon>
-                  </IconButton>
-                  <IconButton size="small" color="inherit" aria-label="mode">
-                      <BrightnessAutoIcon></BrightnessAutoIcon>
-                  </IconButton>
-                  <IconButton size="small" color="inherit" aria-label="mode">
-                      <InfoIcon></InfoIcon>
-                  </IconButton>
-                  <div className="grow"></div>
-                  <Button color="inherit"><FullScreenIcon></FullScreenIcon>&nbsp;Full Screen</Button>
-              </Toolbar>
-          </AppBar>
-      </Box>
+    const [ctx, setCtx] = React.useState({isPlatformDarkMode: false});
 
-      <main className="flex min-h-screen flex-row items-center justify-center p-4">
-          <Button variant="outlined">Hello World</Button>
-      </main>
+    const toggleTheme = () => {
+        setCtx({isPlatformDarkMode: ctx.isPlatformDarkMode !== true});
+    }
 
-    </div>
-  );
+    return (
+        <div>
+            <AppCtx.Provider value={ctx}>
+                <ThemeProvider theme={theme}>
+                    <Box>
+                        <AppBar position="static" sx={{ bgcolor: ctx.isPlatformDarkMode ? theme.palette.primary.dark : theme.palette.primary.light }}>
+                            <Toolbar className="space-x-5">
+                                <IconButton size="small" color="inherit" aria-label="add">
+                                    <AddIcon />
+                                </IconButton>
+                                <IconButton size="small" color="inherit" aria-label="order">
+                                    <ReorderIcon></ReorderIcon>
+                                </IconButton>
+                                <IconButton size="small" color="inherit" aria-label="cloud">
+                                    <CloudIcon></CloudIcon>
+                                </IconButton>
+                                <IconButton size="small" color="inherit" aria-label="mode" onClick={toggleTheme}>
+                                    <BrightnessAutoIcon></BrightnessAutoIcon>
+                                </IconButton>
+                                <IconButton size="small" color="inherit" aria-label="mode">
+                                    <InfoIcon></InfoIcon>
+                                </IconButton>
+                                <div className="grow"></div>
+                                <Button color="inherit"><FullScreenIcon></FullScreenIcon>&nbsp;Full Screen</Button>
+                            </Toolbar>
+                        </AppBar>
+                    </Box>
+
+                    <main className="flex min-h-screen flex-row items-center justify-center p-4">
+                        <Button variant="outlined">Hello World</Button>
+                    </main>
+                </ThemeProvider>
+            </AppCtx.Provider>
+        </div>
+    );
 }
 
 export default App;
